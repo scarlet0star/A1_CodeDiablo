015e7e73e130f51410328ded458caa9ad54f3086
Lee Juneyoung <wnsdud318@gmail.com>
2023-03-30 18:14:15 +0900

    character test

--- character.py
+++ character.py
@@ -4,87 +4,39 @@ from equip import Equip
 # 초기 카드 정보를 초기화할때 json의 초기 카드 id 리스트를 받아와 equip 내의 card 리스트를 구성합니다.
 # __str__을 통해 캐릭터의 정보를 출력할 수 있습니다.
 
+
 class Character():
 
-    def __init__(self, card_instances, **kwargs):
+    def __init__(self, **kwargs):        
+        self._classname = kwargs.get("class_name", "NONE")
+        self._lv = kwargs.get("lv", 1)
+
         self._max_hp = kwargs.get("max_hp", 30)
         self._hp = kwargs.get("hp", 30)
-        self._action = kwargs.get("action", 3)
+        
+        self._max_mana = kwargs.get("max_mana", 30)        
+        self._mana = kwargs.get("mana", 30)
 
-        self._speed = kwargs.get("speed", 1)
-        self._power = kwargs.get("power", 5)
-        self._name = kwargs.get("name", "NONE")
-        self._equip = Equip()
+        self._str = kwargs.get("str", 5)
+        self._dex = kwargs.get("dex", 5)
+        self._int = kwargs.get("int", 5)
+        self._will = kwargs.get("will", 5)
+        self._luck = kwargs.get("luck", 5)
 
-        card_ids = kwargs.get("cards", [])
-        for card_id in card_ids:
-            self._equip.add_card(card_instances[str(card_id)])
+        self._exp = kwargs.get("exp", 0)
 
     def __str__(self):
-        status = f"캐릭터 이름: {self.name}\n"
-        status += f"최대 HP: {self.max_hp}\n"
-        status += f"현재 HP: {self.hp}\n"
-        status += f"액션 포인트: {self.action}\n"
-        status += f"스피드: {self.speed}\n"
-        status += f"파워: {self.power}\n"
+        status = f"캐릭터 클래스: {self._classname}\n"
+        status += f"캐릭터 LV: {self._classname}\n"
+        status += f"최대 HP: {self._max_hp}\n"
+        status += f"현재 HP: {self._hp}\n"
+        status += f"최대 마나: {self._max_mana}\n"
+        status += f"현재 마나: {self._mana}\n"
+        status += f"힘: {self._str}\n"
+        status += f"민첩: {self._dex}\n"
+        status += f"지능: {self._int}\n"
+        status += f"의지: {self._will}\n"
+        status += f"운: {self._luck}\n"
+        status += f"경험치: {self._exp}\n"
+
         return status
-
-    @property
-    def max_hp(self):
-        return self._max_hp
-
-    @max_hp.setter
-    def max_hp(self, value):
-        if value < 0:
-            self._max_hp = 1
-        else:
-            self._max_hp = value
-
-    @property
-    def hp(self):
-        return self._hp
-
-    @hp.setter
-    def hp(self, value):
-        if value < 0:
-            self._hp = 0
-        elif value > self.max_hp:
-            self._hp = self.max_hp
-        else:
-            self._hp = value
-
-    @property
-    def action(self):
-        return self._action
-
-    @action.setter
-    def action(self, value):
-        self._action = value
-
-    @property
-    def speed(self):
-        return self._speed
-
-    @speed.setter
-    def speed(self, value):
-        self._speed = value
-
-    @property
-    def power(self):
-        return self._power
-
-    @power.setter
-    def power(self, value):
-        self._power = value
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self,value):
-        self._name = value
-
-    @property
-    def equip(self):
-        return self._equip
--- screen.py
+++ screen.py
@@ -143,3 +143,215 @@ stage = {
         "logic": battle_stage
         }
 }
+# from battle import Battle
+# from character import Character
+# from main import create_card_instance, enemies
+
+# # 만약 여유가 있다면 텍스트는 따로 저장할 예정입니다.
+
+# start = '''김성광의 게임에 오신것을 환영합니다.\n캐릭터를 생성하거나 기존에 생성한 캐릭터를 통해 몬스터와 전투를 할 수 있습니다.'''
+
+# start_select = ["[ 캐릭터 생성하기 ]",
+#                 "[ 캐릭터 불러오기 ]",
+#                 "[ 게임 종료하기 ]"]
+
+# character_gen = "생성하고 싶은 캐릭터의 이름을 정해주세요."
+
+# load = "기존에 생성한 캐릭터를 불러오고 있습니다."
+
+# main = "메인 화면입니다. 하고 싶은 활동을 선택해주세요"
+
+# main_select = ["[ 장비 확인하기 ]",
+#                "[ 캐릭터 확인하기 ]",
+#                "[ 콜로세움으로 진입 ]",
+#                "[ 게임 종료하기 ]",]
+
+# battle = "콜로세움에 오신 것을 환영합니다. 상대할 적을 골라주세요."
+
+# battle_select = ["[ 고통스러워 하는 개발자 ]",
+#                  "[ 비슷한 상대 ]"]
+
+
+# # 스테이지 넘버를 받아 특정 스테이지의 텍스트를 출력합니다.
+# # return으로 해당 stage의 선택지 텍스트를 리턴합니다.
+
+# def screen_output(stage_num):
+#     stage_text = stage[stage_num]
+#     print(stage_text["text"] if stage_text["text"] != None else "")
+
+#     return stage_text["select"]
+
+# # 리턴된 선택지 리스트를 받아와서 하나씩 출력합니다.유저의 input을 리턴받습니다. 
+# # 이때 리스트 혹은 문자열 판별을 isinstance를 이용합니다. 이후 출력을 따로 받습니다.
+
+# def get_user_input(select):
+#     if isinstance(select, list):
+#         for i, choice in enumerate(select):
+#             print(f"{i + 1} | {choice}")
+#         user_input = int(input("\n원하는 선택지는? >> "))
+
+#     elif isinstance(select, str) and select == "input":
+#         user_input = input("\n값을 기입해주세요 >> ")
+
+#     else:
+#         user_input = ""
+
+#     return user_input
+
+# # 시작 스테이지의 선택지 로직입니다. 선택지 로직은 보통 스테이지 넘버를 리턴합니다.
+
+# def start_stage(choice, mainState):
+#     if choice == 1:
+#         return 1
+#     elif choice == 2:
+#         return 2
+#     elif choice == 3:
+#         exit()
+#     else:
+#         print("선택지에 없는 입력을 하셨습니다. 다시 선택해주세요.")
+
+
+# def gen_stage(choice, mainState):
+#     mainState.user.name = choice
+
+#     print(f"\n환영합니다 {choice}!")
+#     print("캐릭터를 성공적으로 생성했습니다. 이제 게임으로 진입합니다.\n")
+#     return 3
+
+
+# def load_stage(choice, mainState):
+#     # json 형태로 저장할 세이브 파일을 불러오는 코드입니다. 예정사항
+#     print("미구현 입니다.")
+#     return 3
+
+
+# def main_stage(choice, mainState):
+#     if choice == 1:
+#         return 5
+#     elif choice == 2:
+#         return 4
+#     elif choice == 3:
+#         return 6
+#     elif choice == 4:
+#         exit()
+#     else:
+#         print("잘못된 값을 입력하셨습니다.\n")
+
+
+# def character_info(choice, mainState):
+#     print(mainState.user)
+#     return 3
+
+
+# def equip_info(choice, mainState):
+#     print(mainState.user.equip)
+#     return 3
+
+# # main.py에서 적을 생성하기 힘들어서 stage.py에서 선택지에 따라 적도 생성합니다
+# # 적을 생성한 이후 전투로 넘어갑니다. battle이 끝나면 다시 메인 선택지(3)으로 넘어갑니다.
+
+# def battle_stage(choice, mainState):
+#     enemy = Character(card_instances=create_card_instance(
+#         False), **enemies[str(choice)])
+
+#     battle = Battle(mainState.user, enemy)
+#     battle.battle(mainState.user, enemy)
+#     return 3
+
+# # 스테이지 정보를 담고있는 딕셔너리입니다. logic에 함수를 담아 main.py에서 불러올 수 있습니다.
+# # python에서는 함수 역시 1급 객체이기에 가능한 일입니다. 
+
+# stage = {
+#     0: {"text": start,
+#         "select": start_select,
+#         "logic": start_stage},
+#     1: {"text": character_gen,
+#         "select": "input",
+#         "logic": gen_stage},
+#     2: {"text": load,
+#         "select": None,
+#         "logic": load_stage},
+#     3: {"text": main,
+#         "select": main_select,
+#         "logic": main_stage
+#         },
+#     4: {"text": None,
+#         "select": "",
+#         "logic": character_info
+#         },
+#     5: {"text": None,
+#         "select": "",
+#         "logic": equip_info
+#         },
+#     6: {"text": battle,
+#         "select": battle_select,
+#         "logic": battle_stage
+#         }
+# }
+
+
+
+def select_display(insert_scription,insert_selector):
+    from pick import pick
+    script = insert_scription
+    select = insert_selector
+
+    out_selector, index = pick(select, script)
+    
+    print(index, out_selector)
+    return index, out_selector
+
+
+# mi = display('김첨지는 맛좋은 김치',['설렁탕','막걸리','팥빙수'])
+
+# print(mi[0])
+
+
+
+import scripts
+from os import system
+# 게임시작
+new_game = [
+            scripts.SCRIPT_TEXT_NEW_GAME_1_1,
+            scripts.SCRIPT_TEXT_NEW_GAME_1_2,
+            scripts.SCRIPT_TEXT_NEW_GAME_1_3,
+            ]
+u_class = [scripts.SCRIPT_TEXT_SELECT_USER_CLASS_WARRIOR,
+           scripts.SCRIPT_TEXT_SELECT_USER_CLASS_WIZARD,
+           scripts.SCRIPT_TEXT_SELECT_USER_CLASS_ARCHER,
+           scripts.SCRIPT_TEXT_SELECT_USER_CLASS_ALCHEMIST]
+u_story = [scripts.SCRIPT_TEXT_PROLOGUE_0_0,
+           scripts.SCRIPT_TEXT_PROLOGUE_0_1,
+           scripts.SCRIPT_TEXT_PROLOGUE_0_2]
+
+u_battle = ['일반공격','스킬','도망치기']
+
+u_continue_select = [scripts.SCRIPT_TEXT_YES,
+                     scripts.SCRIPT_TEXT_NO]
+
+def scription_controller(scription):
+    for i in range(0,len(scription)):
+        system('cls')
+        print('\n'+scription[i]+('\n'*5))
+        input('엔터를 눌러주세요.')
+        
+    
+
+u_menu_select = select_display(scripts.SCRIPT_TEXT_NEW_GAME_TITLE_0_0, new_game)
+# 이름입력
+system('cls') 
+name = input('당신의 이름은 무엇인가요?')
+# 직업선택
+u_class_select = select_display('직업을 선택하세요',u_class)
+# 스토리텔링
+scription_controller(u_story)
+# 전투준비
+print('전투가 시작됨'+('\n'*5))
+# 전투
+print('전투중'+('\n'*5))
+battle_select = select_display('당신의 턴',u_battle)
+# 전투결과
+print('전투가 끝났습니다. 당신은 ~~~ 얻었습니다.')
+# 전투속행 질의
+battel_end = select_display('다음전투를 계속 하시겠습니까?',u_continue_select)
+# 종료
