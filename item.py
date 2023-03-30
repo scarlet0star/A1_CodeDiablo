d786a429c6b77f3b53fe5ee6dea8bacce00e7a71
Yi Yoonsung <lys11556633@gmail.com>
2023-03-30 22:52:35 +0900

    item fix

--- item.py
+++ item.py
@@ -7,38 +7,58 @@ class Item:
         
         self._name = kwargs.get("name")
         self._weight = kwargs.get("weight")
-        self._dmg = kwargs.get("dmg", 30)
-        self._magicdmg = kwargs.get("magicdmg", 30)
-        self._def = kwargs.get("def", 30)
-        self._magicdef = kwargs.get("mana", 30)
+
         
     def __str__(self): 
         
         status = f"장비명: {self._name}\n"
         status = f"무게: {self._weight}\n"
-        status += f"무게없음: {self._weight}\n"
+
+        return status
+    
+class Weapon_Armor(Item):
+    def __init__(self, **kwargs):
+        
+        self._dmg = kwargs.get("dmg", 30)
+        self._magicdmg = kwargs.get("magicdmg", 30)
+        self._def = kwargs.get("def", 30)
+        self._magicdef = kwargs.get("mana", 30)
+        
+    def __str__(self):
+        
         status += f"데미지: {self._dmg}\n"
         status += f"마법데미지: {self._magicdmg}\n"
         status += f"방어력: {self._def}\n"
         status += f"마법방어력: {self._magicdef}\n"
-
+        
         return status
         
 class Potion(Item): # 포션을 먹으면 체력회복이 되는 메서드가 담긴 클래스입니다.
-    def __init__(self,amount):
-        super().__init__(self, **kwargs)
-        self._amount = amount.get("amount", 10)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        self._amount = kwargs.get("amount", 10)
+        self._stat = kwargs.get("stat","hp") # status
     
-    def use(self,Character): # 케릭터가 포션을 사용하는 메서드
-        self.use = Character[self._amount] += self._amount
+    def use(self,Character): # 케릭터가 포션을 사용하는 메서드 (main.py와 연결 )
+        Character[self._stat] += self._amount
         
     def __str__(self):
-        
-        status += f"hp회복량: {self._amount}\n"
-        status += f"mp회복량: {self._amount}\n"
- 
+        status = f"{self._stat} 회복량: {self._amount}\n"
         return status
         
 class Title(Item): # 장착하면 스탯이 증가하는 칭호 메서드가 담긴 클래스입니다.
-    def __init__(self, sum):
-        super().__init__(self, )
+    def __init__(self,**kwargs):
+        super().__init__(self,**kwargs)
+        
+        
+aa = {
+    "name": "1",
+    "weight": 1,
+    "amount": 10,
+    "healmp": 1,
+    "stat":"_str"
+}
+#aa 포션 생성 => 힘을 증가시키는 물약
+potion = Potion(**aa)
+print(potion)
