dd6e43f320cd5cf6a9895287801154a3a323091e
brighto <jychoi1996@gmail.com>
2023-03-31 13:44:17 +0900

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -10,9 +10,9 @@ SCRIPT_TEXT_SELECT_USER_CLASS_ARCHER = '궁수'
 SCRIPT_TEXT_SELECT_USER_CLASS_WIZARD = '마법사'
 SCRIPT_TEXT_SELECT_USER_CLASS_ALCHEMIST = '연금술사'
 SCRIPT_TEXT_SELECT_USER_CLASS_DEVELOP = '[DEBUG]개발자'
-SCRIPT_TEXT_PROLOGUE_0_0 = '어쩌구 저쩌구'
-SCRIPT_TEXT_PROLOGUE_0_1 = '그래서 어떻게 되었고'
-SCRIPT_TEXT_PROLOGUE_0_2 = '시작됨'
+SCRIPT_TEXT_PROLOGUE_0_0 = '이 게임은 마비노기를 오마주 해서 만들어본 게임입니다.'
+SCRIPT_TEXT_PROLOGUE_0_1 = '즐겁게 게임을 진행해주셨으면 좋겠습니다'
+SCRIPT_TEXT_PROLOGUE_0_2 = '감사합니다.'
 SCRIPT_TEXT_STORY_0_0 = '전투가 어쩌구'
 SCRIPT_TEXT_STORY_0_1 = '세상이 어쩌구'
 SCRIPT_TEXT_STORY_0_2 = '그래서 뭘 해야한다.'
@@ -48,6 +48,25 @@ SCRIPT_TEXT_VIEW_EQUIP = "[ 장비 확인하기 ]"
 SCRIPT_TEXT_VIEW_CHAR = "[ 캐릭터 확인하기 ]"
 # SCRIPT_TEXT_STATUS = f'{hp}/{max_hp} {mp}/{max_mp}'
 # SCRIPT_TEXT_STATUS_ALL = f'{hp}/{max_hp} {mp}/{max_mp}'
+SCRIPT_TEXT_0_POTION = "힐러집은 고요하고 평온한 분위기가 감돌고 있습니다. 아름다운 정원과 진한 약초 냄새가 전해집니다. 상처와 질병을 치료할 수 있는 강력한 마법사들이 이곳에서 봉사하고 있습니다."
+SCRIPT_TEXT_1_POTION = "녹색과 백색이 보기 좋게 어우러진 힐러의 옷을 깔끔하게 차려입었다."
+SCRIPT_TEXT_2_POTION = "보통의 남자들과는 달리 길고 풍성하게 늘어져 떡 벌어진 어깨까지 내려오는"
+SCRIPT_TEXT_3_POTION = "그의 머리카락은 한 올 흐트러짐 없이 정성스레 잘 묶여있다."
+SCRIPT_TEXT_0_ARMOR = "연륜이 느껴지는 윤곽이 뚜렷한 얼굴에 그리 길지 않게 기른 검은 수염, 그리고 잘 그을린 구릿빛 피부가 인상적인 남자다."
+SCRIPT_TEXT_1_ARMOR = "뭔가 굵고 낮은 곡조로 콧노래를 흥얼거리고 있는데, 하체에 비해 상체가 잘 발달된 그의 몸은 그가 흥얼거리는 노래의 박자에 맞춰 기분좋게 천천히 흔들거리고 있다."
+
+
+n_armor = [SCRIPT_TEXT_0_ARMOR,
+           SCRIPT_TEXT_1_ARMOR]
+
+n_potion = [SCRIPT_TEXT_0_POTION,
+            SCRIPT_TEXT_1_POTION,
+            SCRIPT_TEXT_2_POTION,
+            SCRIPT_TEXT_3_POTION]
+
+
+
+
 
 u_new_game = [SCRIPT_TEXT_NEW_GAME_1_1,
               SCRIPT_TEXT_NEW_GAME_1_2,
