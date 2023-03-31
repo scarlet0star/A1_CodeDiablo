2d43ad3cc1bb3f9909e32eeb92c8bb0210295324
brighto <jychoi1996@gmail.com>
2023-03-31 13:43:43 +0900

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,22 +1,22 @@
 # from battle import Battle
 # from character import Character
 # from main import enemies
-import time
 import os
-from utils import pick
 import scripts
-from battle import Battle
-from main import Player
-from main import characters
-from character import Character
+from utils import pick , load_files
+from time import sleep
+items = load_files('item.json')
 
+def script_controller(script):
+    for i in range(0, len(script)):
+        os.system('cls')
+        print('\n'+script[i]+('\n'*5))
+        a = input('엔터를 눌러주세요.')
+        sleep(3)
+        
+    
 
 def battle_stage(mainState):
-    enemy_cha = Character(**characters["1"])
-    enemy = Player(**{"propname": "마법사",
-                      "user": enemy_cha})
-    battle = Battle(mainState, enemy)
-    battle.battle()
     return mainState.stage
 
 
@@ -57,6 +57,77 @@ def back(mainState):
     os.system('cls')
     return mainState.stage
 
+def potion_store(mainState):
+    from item import Potion
+    
+    item_key = items["물약"]
+    new_potion = Potion(**item_key)                   # 포션 인스턴스 생성
+    
+    script_controller(scripts.n_potion)                 #힐러집 대사
+    
+    u_potion_cnt = len(mainState._equip.get_potion_list())      
+    store_porion_list = ["물약","[나가기]","현재 물약 개수 : "+str(u_potion_cnt)]
+    n_script = ['마누스 : 필요한 물건을 골라봐.', '마누스 : 더 필요한건 없어?.']
+    n_num = 0
+    
+    while True:
+        
+        u_select, u_index = pick(store_porion_list,n_script[n_num])         # 선택지 생성
+        
+        if u_select == "물약":
+            u_potion_cnt = len(mainState._equip.get_potion_list())
+            mainState._equip.add_item(new_potion)       #캐릭터에 포션 넘기기
+            store_porion_list.pop(2)
+            store_porion_list.append("현재 물약 개수 : "+str(u_potion_cnt))
+        n_num = 1
+        if u_select == "[나가기]":
+            return mainState.stage
+        
+        
+# potion_store(0)
+
+def armory(mainState):
+    from random import randint
+    import item 
+    script_controller(scripts.n_armor)                 #대장간 대사
+    
+    armor_key = items["방어구"]                        # 장비류 인스턴스 생성
+    wep_key = items["무기"]
+    armor = item.Arms(**armor_key)
+    wep = item.Arms(**wep_key)
+    
+    store_porion_list = ["무기","방어구","[나가기]"]
+    n_script = ['퍼거스 : 무기를 고치러 왔나?' , '퍼거스 : 이크! 손이 미끄러졌네... 헐...',"네가 내 운을 다 빨아먹어서 실패한 거야!"]
+    n_num = 0
+    while True:
+        
+        u_select, u_index = pick(store_porion_list,n_script[n_num])     #선택지생성
+        
+        if u_select == "방어구":
+            mainState._equip.add_item(armor)
+            n_num = randint(1,2)
+        elif u_select == "무기":
+            mainState._equip.add_item(wep)
+        if u_select == "[나가기]":
+            return mainState.stage
+# armory(0)
+
+def grocery_store(mainState):
+    script_controller(["잡화점에는 세상의 다양한 물건들이 가득합니다. 이곳에서는 모험에 필요한 도구와 장비를 구입할 수 있습니다. 사장님이 항상 밝은 얼굴로 인사해주며, 고객들과 이야기를 나누고 있습니다."])
+
+def bank(mainState):
+    script_controller(["은행 근처는 번화가의 중심입니다. 사람들이 북적이며, 거리에는 상인들이 자신의 물품을 파는 노점도 있습니다. 이곳에서는 자금을 관리하거나 소중한 물건을 보관할 수 있습니다."])
+
+def hotel(mainState):
+    script_controller(["여관에서는 다양한 모험가들이 쉬고 있습니다. 따뜻한 분위기와 향긋한 음식 냄새가 흐릅니다. 여기에서 편히 쉬며 체력을 회복할 수 있습니다."])
+
+def grave(mainState):
+    script_controller(["묘지는 조용하고 무서운 곳입니다. 차가운 바람이 지나가며, 누군가가 당신을 지켜보고 있는 듯한 기분이 듭니다. 이곳에서는 과거의 영웅들과 전설에 관한 이야기를 찾을 수 있습니다."])
+    
+def field(mainState):
+    script_controller(["목축지는 푸른 초원과 넓은 들판이 펼쳐져 있는 곳입니다. 소와 양 등의 가축들이 평화롭게 먹이를 찾고 있습니다. 이곳에서는 소박한 농부들과 함께 일하며, 농작물과 가축에 대한 지식을 얻을 수 있습니다."])
+
+# grocery_store(0)
 
 action_function = {0: battle_stage,
                    1: view_skill,
@@ -69,15 +140,25 @@ action_function = {0: battle_stage,
 
 selection = ["이동", "행동"]
 
-places = {
-    0: ["[여관]", "여관에서는 다양한 모험가들이 쉬고 있습니다. 따뜻한 분위기와 향긋한 음식 냄새가 흐릅니다. 여기에서 편히 쉬며 체력을 회복할 수 있습니다."],
-    1: ["[묘지]", "묘지는 조용하고 무서운 곳입니다. 차가운 바람이 지나가며, 누군가가 당신을 지켜보고 있는 듯한 기분이 듭니다. 이곳에서는 과거의 영웅들과 전설에 관한 이야기를 찾을 수 있습니다."],
-    2: ["[힐러집]", "힐러집은 고요하고 평온한 분위기가 감돌고 있습니다. 아름다운 정원과 진한 약초 냄새가 전해집니다. 상처와 질병을 치료할 수 있는 강력한 마법사들이 이곳에서 봉사하고 있습니다."],
-    3: ["[잡화점]", "잡화점에는 세상의 다양한 물건들이 가득합니다. 이곳에서는 모험에 필요한 도구와 장비를 구입할 수 있습니다. 사장님이 항상 밝은 얼굴로 인사해주며, 고객들과 이야기를 나누고 있습니다."],
-    4: ["[은행근처]", "은행 근처는 번화가의 중심입니다. 사람들이 북적이며, 거리에는 상인들이 자신의 물품을 파는 노점도 있습니다. 이곳에서는 자금을 관리하거나 소중한 물건을 보관할 수 있습니다."],
-    5: ["[목축지]", "목축지는 푸른 초원과 넓은 들판이 펼쳐져 있는 곳입니다. 소와 양 등의 가축들이 평화롭게 먹이를 찾고 있습니다. 이곳에서는 소박한 농부들과 함께 일하며, 농작물과 가축에 대한 지식을 얻을 수 있습니다."],
-    6: ["[대장간]", "대장간에서는 불꽃이 튀기며 철과 강철이 녹아내립니다. 대장장이가 망치질하며, 새로운 무기와 갑옷을 만들고 있습니다. 이곳에서는 전투에 필요한 강력한 무기와 방어구를 살 수 있습니다"]
-}
+
+p_actions = [
+    "[여관]",
+    "[묘지]", 
+    "[힐러집]",
+    "[잡화점]",
+    "[은행근처]",
+    "[목축지]",
+    "[대장간]",
+]
+
+p_action_function = {0: hotel,
+                    1: grave,
+                    2: potion_store,
+                    3: grocery_store,
+                    4: bank,
+                    5: field,
+                    6: armory,
+                    }
 
 actions = ["[전투]",
            "[전체 스킬 확인]",
@@ -92,17 +173,15 @@ actions = ["[전투]",
 
 def staging(mainState):
     stage_num = mainState.stage
-    place_select, place_subscript = list(zip(*places.values()))
-    print(place_subscript[stage_num])
-    input(" ")
+    place_select, place_subscript = p_actions, p_action_function
     action_value, action_index = pick(
-        selection, f"{place_subscript[stage_num]}\n{mainState.propname}이 할 행동은?")
+        selection, f"{place_select[stage_num]}\n{mainState.propname}이 할 행동은?")
 
     # 이동 선택지 고를시
     if action_index == 0:
         move_value, move_index = pick(place_select, "이동하고 싶은 장소를 선택해주세요")
-        print(f'{move_value.strip("[,]").strip(" ")}로 이동합니다\n')
-        os.system('cls')
+        print(f'{place_select[move_index]}로 이동합니다\n')
+        p_action_function[move_index](mainState)
         return move_index
     # 행동 선택지 고를시
     else:
@@ -112,22 +191,15 @@ def staging(mainState):
         return stage_num
 
 
-def script_controller(script):
-    for i in range(0, len(script)):
-        os.system('cls')
-        print('\n'+script[i]+('\n'*5))
-        input('엔터를 눌러주세요.')
-
-
 def new_game():
     # 이름입력
     os.system('cls')
     u_name = input('당신의 이름은 무엇인가요?')
     # 직업선택
-    _, u_class_select = pick(scripts.u_class, '직업을 선택하세요',)
+    _,u_class_select = pick(scripts.u_class,'직업을 선택하세요')
     # 스토리텔링
     script_controller(scripts.u_story)
-    return u_name, str(u_class_select)
+    return u_name , str(u_class_select)
 
 # # class Battle:
 #     # 전투준비
